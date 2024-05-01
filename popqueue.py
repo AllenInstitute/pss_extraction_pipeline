eee4a4c6506412b7f9f28e2b928ea9e7bb244a53
sharmishtaa <shtaaa@gmail.com>
2024-05-01 14:35:42 -0700

    code for paper

--- popqueue.py
+++ popqueue.py
@@ -4,6 +4,7 @@ from taskqueue import queueable
 from featureExtraction.utils import pss_extraction_utils_updated, taskqueue_utils
 import os
 os.environ['NO_GCE_CHECK'] = 'true'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
 @queueable
 def print_task(txt):
@@ -17,10 +18,12 @@ def stop_function (executed):
     return False
 
 print("About to start processing")
-tq = TaskQueue('sqs://sharmi_pss', region_name="us-west-2", green=False)
+tq = TaskQueue(YOUR_TASK_QUEUE, region_name="us-west-2", green=False)
+
 tq.poll(
-  lease_seconds=int(3000),
+  lease_seconds=int(40000),
   verbose=True, # print out a success message
   tally=True, # count number of tasks completed (fq only!)
   stop_fn  = stop_function
 )
+
